9554a30609079ff3447bf041f4efd76ef0ea2065
Daniel Kurz <danieljkurz@gmail.com>
2020-09-28 14:03:50 +1000

    Created a users.csv database that is populated by the createAccount method

--- src/main.rb
+++ src/main.rb
@@ -1,4 +1,6 @@
 require 'tty-prompt'
+require 'json'
+require 'csv'
 
 prompt = TTY::Prompt.new
 
@@ -34,7 +36,22 @@ end
 
 def createAccount
     puts "Please create a username"
+    input_username = gets.chomp
+    puts "Please create a password"
+    input_password = gets.chomp
+    user_account = { input_username: input_password }
+    CSV.open("users.csv" , "a") do |user|
+        user << [input_username, input_password]
+    end
+
+
+    # CSV.open("cats.csv", "w") do |csv|
+#    csv << ["color", "qty"]
+#    cats.to_a.each { |cat| csv << cat }
+# end
 end
+
+
 
 list_of_users = [
     GeneralUser.new("Steph", "pass"),
@@ -43,10 +60,16 @@ list_of_users = [
 ]
 
 
-prompt.select('Please select an option:') do |menu|
+login_menu = prompt.select('Please select an option:') do |menu|
   
     menu.choice 'Create an Account'
     menu.choice 'Sign-In'
     menu.choice 'Exit'
-  end
+end
+
+case login_menu
+when "Create an Account"
+    createAccount
+end
+    
 
